894aba6f3e9fdf2c335ca7dade06815479fb1be5
akhila2711 <akhilakhi112799@gmail.com>
2025-05-22 05:28:24 +0530

    mongoose

---
+++ app.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+    console.log("Connected to MongoDB");
+}).catch(() => {
+    console.error("Error connecting to MongoDB");
+})
