e73cb85c63328cefbadfd1fc82269bee67e15faa
akhila2711 <akhilakhi112799@gmail.com>
2025-05-22 05:33:10 +0530

    mongoose

--- modal/userModal.js
+++ modal/userModal.js
@@ -1,1 +1,6 @@
 const {default:mongoose} = require("mongoose")
+const mangoose=require('mongoose')
+const userSchema = new mangoose.Schema({
+    name: String,age:Number
+})
+const userModel = mangoose.model('user', userSchema)
